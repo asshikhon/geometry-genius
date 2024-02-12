e7df132d37ffecb453b6d6467b762766ef7af15c
asshikhon <asshikhon@gmail.com>
2024-02-13 04:28:58 +0600

    Parallelogram card added

---
+++ script/parallelogram.js
@@ -0,0 +1,32 @@
+function calculateParallelogramArea(){
+
+const base = getInputValueById('parallelogram-base')
+// console.log('base value', base);
+
+const height = getInputValueById('parallelogram-height')
+// console.log('height value', height);
+
+const area = base * height;
+console.log('area of the parallelogram:', area);
+
+// display area 
+
+setInnerTextById('parallelogram-area', area)
+
+}
+
+
+
+function getInputValueById(inputFieldId){
+const inputField = document.getElementById(inputFieldId);
+const inputValueText = inputField.value;
+const inputValue = parseFloat(inputValueText);
+return inputValue;
+}
+
+
+
+function setInnerTextById(elementId, area){
+    const element = document.getElementById(elementId);
+    element.innerText = area;
+}
