20ba1e082b141acca4164ee390e96603715eb846
asshikhon <asshikhon@gmail.com>
2024-02-13 03:10:32 +0600

    added second card

--- script/rectangle.js
+++ script/rectangle.js
@@ -1,3 +1,20 @@
 function calculateRectangleArea(){
-    console.log('calculateRectangleArea');
+
+const lengthInput = document.getElementById('rectangle-lenght');
+const lengthText = lengthInput.value;
+const length =parseFloat(lengthText);
+    console.log(length);
+
+    // width
+const widthInput = document.getElementById('rectangle-width');
+const widthText = widthInput.value;
+const width = parseFloat(widthText)
+console.log(width);
+
+// calculate
+const area = length * width;
+console.log('area of the rectangle:', area);
+// display area 
+const rectangleAreaSpan = document.getElementById('rectangle-area');
+rectangleAreaSpan.innerText = area;
 }
